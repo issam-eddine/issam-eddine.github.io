99d8013cac18747d6fd051bab3bbd1141f75b130
issam-eddine <151002143+issam-eddine@users.noreply.github.com>
2025-10-13 03:49:17 +0000

    Deploying to gh-pages from @ issam-eddine/issam-eddine.github.io@65573297c025450ab1a2f09161fe25559ef87c54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A collection of my projects.",
+          description: "a collection of my projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
